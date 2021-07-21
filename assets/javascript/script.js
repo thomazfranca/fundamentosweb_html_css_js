114533f1c1fed002648a5527c44778c85b07d1db
thomazfranca <trsf1990@gmail.com>
2021-07-20 21:45:20 -0300

    Utilizando JS

---
+++ assets/javascript/script.js
@@ -0,0 +1,41 @@
+/*
+
+Case sentitive
+por tag: getElementeByTagName()
+por Id: getElementById()
+por nome: getElementsByName()
+por classe: getElementsByClass
+por seletor: querySelector()
+*/
+
+let nome = window.document.getElementById('nome')
+let email = document.querySelector('#email')
+let assunto = document.querySelector('#assunto')
+
+nome.style.width ='100%'
+email.style.width = '100%'
+
+function validaNome (){
+    let txtNome = document.querySelector('#txtNome')
+    if (nome.value.length < 3) {
+        txtNome.innerHTML = "Nome Inválido"
+        txtNome.style.color = "red"
+    } else {
+        txtNome.innerHTML = "Nome Válido"
+        txtNome.style.color ="green"
+    }
+}
+
+function validaEmail(){
+    let txtEmail = document.querySelector('#email')
+    if (email.value.indexOf('@') == -1) {
+        txtEmail.innerHTML = "Email invádlido"
+        txtEmail.style.color ="red"
+    } else {
+        txtEmail.innerHTML = "Email Válido"
+        txtEmail.style.color ="green"
+    }
+}
+
+
+
